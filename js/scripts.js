28a4058e8e2545e9e52566bce70fabf269975671
abkendal <kendal.adam@mail.com>
2015-07-30 14:48:29 -0400

    Create getTweets, compileTweets, and analyzeSentiment functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,68 @@
-var twitterApp = {};
+// Stats:
+// 	-Average sentiment over last 100 texts
+// 	-tweets/day (search last 500 tweets, look at the date of the oldest then do some math)
+// 	-
+
+
+
+var app = {};
 
 
 app.getTweets = function(){
 	$.ajax({
-		url : 'localhost/twitterApp/twitter_json.php',
+		url : 'http://localhost/twitterApp/tweets_json.php',
 		type: 'GET',
 		dataType: 'jsonp',
 		data: {
-			twitter_path: '1.1/search/tweets.json',
-			q: 'Toronto'
+			twitter_path: '1.1/search/tweets.json',		
+			q: 'Nickelback',
+			lang: 'en',
+			result_type: 'recent',
+			count: 10
+		},
+		success : function(tweet){
+			app.compileTweets(tweet);
+		}
+	})
+};
+
+app.compileTweets = function(tweets){
+	var compiledTweet = '';
+	$.each(tweets, function(i, tweetData){
+		$.each(tweetData, function(j, tweet){
+			if (tweet.text) {
+				compiledTweet = compiledTweet + " "+ tweet.text;
+			}
+		})
+	})
+	// Remove all twitter handles, urls and dashes. 
+	compiledTweet = compiledTweet.replace(/\@\S+/g,"").replace(/\http\S+/g,"").replace(/\-\S+/g,"");
+	console.log(compiledTweet);
+	app.analyzeSentiment(compiledTweet);
+};
+
+app.analyzeSentiment = function(tweetText){
+	$.ajax({
+		url: 'http://access.alchemyapi.com/calls/text/TextGetTextSentiment',
+		type: 'GET',
+		dataType: 'json',
+		data: {
+			apikey: '5f798feb1fb9ee663aa54a9e10a5d9ff179408c0',
+			outputMode: 'json',
+			text:tweetText
+		},
+		success: function(){
+			
 		}
 	})
 };
 
 
 
-twitterApp.init = funtion (){
+app.init = function (){
 	app.getTweets();
 };
 
 $(function(){
-	twitterApp.init();
+	app.init();
 });
